8f941098c4618c4d7ba8988e4607cd8f99a97b9b
Aheedjo <maishanu.ahijo@yahoo.com>
2023-08-25 14:15:21 +0100

    Enabled CORS for frontend API access

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import cors from "cors"
 import express from "express";
 import bodyParser from "body-parser";
 import routes from "./route/index.js"
@@ -7,6 +8,7 @@ const app = express();
 connectDB();
 const port = 3050;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
